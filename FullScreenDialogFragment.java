66882fa0a30b8e98894eefede625b20cf2e4fa10
Kevin Córdova <42162925+kcordovas@users.noreply.github.com>
2020-04-25 13:18:11 -0500

    Add files via upload

---
+++ FullScreenDialogFragment.java
@@ -0,0 +1,41 @@
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FullScreenDialogFragment extends DialogFragment {
+
+    public FullScreenDialogFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullDialogFragmentLight);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mViewMain = inflater.inflate(R.layout.fragment_full_screen_dialog_layout, container, false);
+        return mViewMain;
+    }
+	
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+        }
+    }
+}
